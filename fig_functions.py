0a27809f30bd8e4f2c1fecef4da6ae049443133d
Kevin Gomez <kgomez81@email.arizona.edu>
2022-01-29 13:25:55 -0700

    adding changes to setup pipeline for data and graphs

--- fig_functions.py
+++ fig_functions.py
@@ -17,7 +17,6 @@ import scipy.optimize as opt
 # FUNCTIONS TO GET QUANTITIES FROM DESAI AND FISHER 2007
 # *****************************************************************************
 
-
 def get_vDF(N,s,U):
     # Calculates the rate of adaptation v, derived in Desai and Fisher 2007
     # for a given population size (N), selection coefficient (s) and beneficial
@@ -55,41 +54,74 @@ def get_qDF(N,s,U):
     return q
 
 #------------------------------------------------------------------------------
-    
-def get_extinction_class(b,d,sa):
-    # function calculates the class for which population size has a negative 
-    # growth rate in the Bertram & Masel 2019 lottery model
+
+def get_vDF_pfix(N,s,U,pFix):
+    # Calculates the rate of adaptation v, using a heuristic version of Desai 
+    # and Fisher 2007 argument, but without the asumption that p_fix ~ s, i.e. 
+    # for a given population size (N), selection coefficient (s) and beneficial
+    # mutation rate (U) and finally p_fix
     #
-    # inputs:
-    # b - juvenile birth rate
-    # d - minimum death rate of optimal genotype
-    # sa - selection coefficient of beneficial mutations in "d" trait
+    # Inputs:
+    # N - population size
+    # s - selection coefficient
+    # U - beneficial mutation rate
+    # pFix -  probability of fixation
     #
     # Output: 
-    # i_ext - smallest absolute fitness class with negative growth
-    #
+    # v - rate of adaptation
+        
+    v = s**2*(2*np.log(N*pFix)-np.log(s/U))/(np.log(s/U)**2)
     
-    i_ext = int(np.ceil((1-d/(b+1))/(sa*d)))
-    
-    return i_ext
+    return v
+
+
 #------------------------------------------------------------------------------
 
-def get_class_death_rate(d,sa,i):
-    # returns death rate of class i given parameters d,sa. Note need to have 
-    # i <= ceiling(i_ext)
+def get_vSucc_pFix(N,s,U,pFix):
+    # Calculates the rate of adaptation v for the successional regime
     #
-    # inputs:
-    # d - minimum death rate of optimal genotype
-    # sa - selection coefficient of beneficial mutations in "d" trait
-    # i - absolute fitness class
+    # Inputs:
+    # N - population size
+    # s - selection coefficient
+    # U - beneficial mutation rate
+    # pFix - probability of fixation
     #
     # Output: 
-    # di - death rate of absolute fitness class i
+    # v - rate of adaptation
+        
+    v = N*U*pFix*s
+    
+    return v
+
+#------------------------------------------------------------------------------
+
+def get_rateOfAdapt_v(N,s,U,pFix):
+    # Calculates the rate of adaptation v, but checks which regime applies.
+    #
+    # Inputs:
+    # N - population size
+    # s - selection coefficient
+    # U - beneficial mutation rate
+    # pFix -  probability of fixation
+    #
+    # Output: 
+    # v - rate of adaptation
     #
     
-    di = d/(1-i*sa*d)
+    # Calculate mean time between establishments
+    Test = N*U*pFix
     
-    return di
+    # Calculate mean time of sweep
+    Tswp = (1/s)*np.log(N*pFix)
+    
+    # calculate rate of adaptation based on regime
+    if (Test < Tswp):
+        v = get_vSucc_pFix(N,s,U,pFix)
+    else:
+        # this needs to be divided into multiple mutations and diffusion regime
+        v = get_vDF_pFix(N,s,U,pFix)
+    
+    return v
 
 #------------------------------------------------------------------------------
 
@@ -138,22 +170,124 @@ def get_eq_pop_density(b,d,sa,i,option):
 
 #------------------------------------------------------------------------------
     
-def get_c_selection_coefficient(b,y,sr):
+def get_extinction_classes(b,dOpt,sa):
+    # function calculates the class for which population size has a negative 
+    # growth rate in the Bertram & Masel 2019 lottery model
+    #
+    # inputs:
+    # b - juvenile birth rate
+    # dOpt - death term of optimal genotype
+    # sa - selection coefficient of beneficial mutations in "d" trait
+    #
+    # Output: 
+    # iExt - largest fitness class after which there is negative growth in pop size
+    # dMax - largest death term after which there is negative growth in pop size
+    # di - list of death terms corresponding to the discrete absolute fit classes
+    
+    # Theoretical derivations show that maximum size of death term is given by 
+    # value below. See Appendix A in manuscript.
+    dMax = b+1
+    
+    # Recursively calculate set of absolute fitness classes 
+    di = [dOpt]
+    iMax = 0                            
+    while (di[-1] < dMax):
+        # loop until stop at dMax or greater reached
+        di = di+[di[i-1]*(1+sa*(d[i-1]-1))]
+        iMax = iMax + 1
+    
+    di = np.asarray(di)
+
+    return [iExt,dMax,di]
+
+#------------------------------------------------------------------------------
+    
+def get_c_selection_coefficient(b,y,cr,d_i):
     # Calculate the "c" selection coefficient for the Bertram & Masel variable 
     # density lottery model for choice of ci = (1+sr)^i
     #
     # Inputs:
     # b - juvenile birth rate
     # y - equilibrium population density
-    # sr - increase to ci from a single beneficial mutation is (1+sr)
+    # cr - increase to ci from a single beneficial mutation is (1+cr)
+    # d_i - death term of the mutant class
     #
     # Output: 
-    # eff_sr - selection coefficient of beneficial mutation in "c" trait
+    # sr - selection coefficient of beneficial mutation in "c" trait
     #
 
-    eff_sr = sr*(1-y)*(1-(1+b*y)*np.exp(-b*y))/(y+(1-y)*(1-np.exp(-b)))*(b*y-1+np.exp(-b*y))/(b*y*(1-np.exp(-b*y))+sr*(1-(1+b*y)*np.exp(-b*y)))
+    # calculate rate of increase in frequency per iteration
+    r_rel = cr*(1-y)*(1-(1+b*y)*np.exp(-b*y))/(y+(1-y)*(1-np.exp(-b)))*(b*y-1+np.exp(-b*y))/(b*y*(1-np.exp(-b*y))+cr*(1-(1+b*y)*np.exp(-b*y)))
     
-    return eff_sr
+    # re-scale time to get rate of increase in frequency per generation
+    tau_i = myfun.get_iterationsPerGeneration(d_i)
+    
+    sr = r_rel*tau_i
+    
+    return sr
 
+#------------------------------------------------------------------------------
+    
+def get_iterationsPerGeneration(d_i):
+    # Calculate the "c" selection coefficient for the Bertram & Masel variable 
+    # density lottery model for choice of ci = (1+cr)^i
+    #
+    # Inputs:
+    # d_i - death term of the mutant class
+    #
+    # Output: 
+    # tau_i - mean number of iterations per generation (i.e. average generation time in terms of iteration)
+    #
+    
+    tau_i = 1/(d_i-1)
+    
+    return tau_i
 
+#------------------------------------------------------------------------------
 
+def read_parameterFile(readFile,paramList):
+    # This function reads the parameter values a file and creates a dictionary
+    # with the parameters and their values 
+
+    # create array to store values
+    paramValue = np.zeros([len(paramList),1])
+    
+    # read values from csv file
+    with open(readFile,'r') as csvfile:
+        parInput = csv.reader(csvfile)
+        for (line,row) in enumerate(parInput):
+            paramValue[line] = float(row[0])
+    
+    # create dictionary with values
+    paramDict = dict([[paramList[i],paramValue[i]] for i in len(paramValue)])
+    
+    return paramDict
+
+#------------------------------------------------------------------------------
+    
+def read_pFixOutputs(readFile,nStates):
+    # This function reads the output file containing the estimated pfix values 
+    # simulations and stores them in an array so that they can be used in 
+    # creating figures.
+    #
+    # pFix values for absolute fitness classes should specified beginning from
+    # the optimal class, all the way up to the extinction class or greater. The
+    # code below will only read up to the extinction class.
+    #
+    # Inputs:
+    # readFile - name of csv file that has the estimated pFix values.
+    #
+    # Outputs:
+    # pFixValues - set of pFix values, beginning from the optimal absolute up to
+    #               the extinction class
+    
+    # create array to store pFix values
+    pFixValues = np.zeros([nStates,1])
+    
+    # read values from csv file
+    with open(readFile,'r') as csvfile:
+        pfixOutput = csv.reader(csvfile)
+        for (line,row) in enumerate(pfixOutput):
+            pFixValues[line] = float(row[0])
+    
+    return pFixValues

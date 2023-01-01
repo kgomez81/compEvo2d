341622a55c365c34c769dd4be0a68409446e532e
Kevin Gomez <kgomez81@arizona.edu>
2022-12-31 18:55:50 -0700

    Initial updates for new figures.

--- figureScripts/fig_MarkovChain_VaVeVrIntersectDRE.py
+++ figureScripts/fig_MarkovChain_VaVeVrIntersectDRE.py
@@ -12,7 +12,10 @@ Created on Sat Mar 05 15:30:20 2022
 import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
-import evo_library as myfun            # my functions in a seperate file
+
+import sys
+sys.path.insert(0, 'D:\\Documents\\GitHub\\compEvo2d')
+from evoLibraries import evo_library as myfun            # my functions in a seperate file
 
 # --------------------------------------------------------------------------
 # Calculate Markov Chain Evolution Parameters - Panel (A)
@@ -35,15 +38,14 @@ import evo_library as myfun            # my functions in a seperate file
 #
 
 # The parameter file is read and a dictionary with their values is generated.
-paramFile = 'inputs/evoExp_DRE_02_parameters.csv'
+workDir = 'D:/Documents/GitHub/compEvo2d/'
+paramFile = workDir + 'inputs/evoExp_DRE_02_parameters.csv'
 params = myfun.read_parameterFileDRE(paramFile)
 
 # Calculate absolute fitness state space. This requires specificying:
 # dMax  - max size of death term that permits non-negative growth in abundances
 # di    - complete of death terms of the various absolute fitness states
-# iMax  - max number of beneficial mutations to show in plots
-iMaxClass = 500
-[dMax,di,iMax] = myfun.get_absoluteFitnessClassesDRE(params['b'],params['dOpt'],params['alpha'],iMaxClass)
+[dMax,di,iMax] = myfun.get_absoluteFitnessClassesDRE(params['b'],params['dOpt'],params['alpha'])
 
 # set root solving option for equilibrium densities
 # (1) low-density analytic approximation 
@@ -72,14 +74,16 @@ ax1.scatter(state_i,va_i,color="blue",s=8,label=r'$v_a$')
 ax1.scatter(state_i,vr_i,color="red",s=8,label=r'$v_r$')
 
 # axes and label adjustements
+iMax = 100
 ax1.set_xlim(0,iMax)
-ax1.set_ylim(0,4.0e-5)    # 2,5e04 ~ 1.5*max([max(va_i),max(vr_i)])
+ax1.set_ylim(0,5.0e-4)    # 2,5e04 ~ 1.5*max([max(va_i),max(vr_i)])
 
-ax1.set_xticks([100*i for i in range(0,iMax/100+1)])
-ax1.set_xticklabels(["" for i in range(0,iMax/100)],fontsize=16)
+xTickMax = int(iMax/100+1)
+ax1.set_xticks([100*i for i in range(0,xTickMax)])
+ax1.set_xticklabels(["" for i in range(0,xTickMax)],fontsize=16)
 
-ax1.set_yticks([1e-5*i for i in range(0,5)])
-ax1.set_yticklabels([str(1.0*i) for i in range(0,5)],fontsize=16)
+# ax1.set_yticks([1e-5*i for i in range(0,5)])
+# ax1.set_yticklabels([str(1.0*i) for i in range(0,5)],fontsize=16)
 
 #ax1.set_xlabel(r'Absolute fitness class',fontsize=20,labelpad=8)
 ax1.set_ylabel(r'Rate of adaptation',fontsize=20,labelpad=8)
@@ -97,10 +101,10 @@ ax1.annotate("", xy=(290,1.2e-5), xytext=(330,1.2e-5),arrowprops={'arrowstyle':'
 # Recalculate Markov Chain Evolution Parameters - Panel (B)
 # --------------------------------------------------------------------------
 
-paramFile = 'inputs/evoExp_DRE_01_parameters.csv'
+paramFile = workDir + 'inputs/evoExp_DRE_01_parameters.csv'
 params = myfun.read_parameterFileDRE(paramFile)
 
-[dMax,di,iMax] = myfun.get_absoluteFitnessClassesDRE(params['b'],params['dOpt'],params['alpha'],iMaxClass)
+[dMax,di,iMax] = myfun.get_absoluteFitnessClassesDRE(params['b'],params['dOpt'],params['alpha'])
 
 # calculate the state space
 [state_i,Ua_i,Ur_i,eq_yi,eq_Ni,sr_i,sa_i] = myfun.get_MChainPopParametersDRE(params,di,iMax,yi_option)
@@ -122,14 +126,16 @@ ax2.scatter(state_i,va_i,color="blue",s=8,label=r'$v_a$')
 ax2.scatter(state_i,vr_i,color="red",s=8,label=r'$v_r$')
 
 # axes and label adjustements
+iMax = 100
 ax2.set_xlim(0,iMax)
-ax2.set_ylim(0,4.0e-5)       # 1.5*max([max(va_i),max(vr_i)])
+ax2.set_ylim(0,5.0e-4)       # 1.5*max([max(va_i),max(vr_i)])
 
-ax2.set_xticks([100*i for i in range(0,iMax/100+1)])
-ax2.set_xticklabels([str(100*i) for i in range(0,iMax/100+1)],fontsize=16)
+xTickMax = int(iMax/100+1)
+ax2.set_xticks([100*i for i in range(0,xTickMax)])
+ax2.set_xticklabels([str(100*i) for i in range(0,xTickMax)],fontsize=16)
 
-ax2.set_yticks([1e-5*i for i in range(0,5)])
-ax2.set_yticklabels([str(1.0*i) for i in range(0,5)],fontsize=16)
+# ax2.set_yticks([1e-5*i for i in range(0,5)])
+# ax2.set_yticklabels([str(1.0*i) for i in range(0,5)],fontsize=16)
 
 ax2.set_xlabel(r'Absolute fitness class',fontsize=20,labelpad=8)
 ax2.set_ylabel(r'Rate of adaptation',fontsize=20,labelpad=8)
@@ -142,9 +148,9 @@ ax2.annotate("", xy=(230,0.5e-5), xytext=(190,0.5e-5),arrowprops={'arrowstyle':'
 ##plt.text(-78,0.29e-4,r'$i^*=84$',fontsize = 18)
 ##plt.text(-78,0.10e-4,r'$i_{ext}=180$',fontsize = 18)
 ##plt.text(-190,2.58e-4,r'$\times 10^{-4}$', fontsize = 20)
-plt.text(15,7.95e-5,r'(A)', fontsize = 22)
-plt.text(15,3.7e-5,r'(B)', fontsize = 22)
+# plt.text(15,7.95e-5,r'(A)', fontsize = 22)
+# plt.text(15,3.7e-5,r'(B)', fontsize = 22)
 
 # save figure
 plt.tight_layout()
-fig1.savefig('figures/fig_MChain_VaVeVrIntersectionDRE.pdf')
+fig1.savefig(workDir + 'figures/MainDoc/fig_MarkovChain_VaVeVrIntersectionDRE.pdf')

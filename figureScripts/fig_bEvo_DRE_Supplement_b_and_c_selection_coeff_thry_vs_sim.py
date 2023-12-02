d382cc4febb8f65a82fecc9fd690da62bf798f50
Kevin Gomez <kgomez81@arizona.edu>
2023-12-02 13:33:46 -0700

    new plot for supplement to show selection coefficients.

---
+++ figureScripts/fig_bEvo_DRE_Supplement_b_and_c_selection_coeff_thry_vs_sim.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 25 12:39:02 2022
+
+@author: dirge
+"""
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 11:56:52 2020
+@author: Kevin Gomez, Nathan Aviles
+Masel Lab
+see Bertram, Gomez, Masel 2016 for details of Markov chain approximation
+see Bertram & Masel 2019 for details of lottery model
+"""
+
+# --------------------------------------------------------------------------
+#                               Libraries   
+# --------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+import sys
+sys.path.insert(0, 'D:\\Documents\\GitHub\\compEvo2d')
+
+from evoLibraries import evoObjects as evoObj
+from evoLibraries.MarkovChain import MC_DRE_class as mcDRE
+
+from matplotlib.lines import Line2D
+
+

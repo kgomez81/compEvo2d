3dd8adc0f3b073cb1b6718a4ecb44c083e947123
Kevin Gomez <kgomez81@arizona.edu>
2025-03-16 19:40:24 -0700

    added comments to SIM DRE class file.

--- evoLibraries/SimRoutines/SIM_DRE_class.py
+++ evoLibraries/SimRoutines/SIM_DRE_class.py
@@ -235,7 +235,139 @@ class simDREClass(sim.simClass):
 
         return fitMap
     
-    #%% ------------------------------------------------------------------------
-    # Specific class methods
-    # --------------------------------------------------------------------------
+    # #%% ------------------------------------------------------------------------
+    # # SIM class inherited methods
+    # # --------------------------------------------------------------------------
     
+    # #---------------------------------------------------------------------------
+    # # Class methods
+    # # --------------------------------------------------------------------------
+    
+    # def run_evolutionModel(self):
+    #     # main function to run evolutionary model, iterations continue until
+    #     # the population is extinct or tmax is reached.
+
+    # # --------------------------------------------------------------------------
+    # # Methods for Full Stochastic Model 
+    # #------------------------------------------------------------------------------
+    
+    # def run_competitionPhase(self):
+    #     # run_competitionPhase() generates the set of juvenilees and 
+    #     # simulates competition to determine the number of adults prior
+    #     # to the death phase.
+    
+    # #------------------------------------------------------------------------------
+    
+    # def run_deathPhase(self):
+    #     # run_deathPhase() gernates the set of new and remaining adults 
+    #     # following death.
+
+    # #------------------------------------------------------------------------------
+    
+    # def get_stochasticFitnessClasses(self):
+    #     # get_stochasticFitnessClasses() identifies the set of genotypes that are 
+    #     # below the stochastic threshold, whose abundances will not be modeled 
+    #     # deterministically.
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_arrayMap(self,A):
+    #     # Used to flattens 2d arrays so that they can be used by SIM_functions
+    #     # the map allows calculated values from functions to be placed back 
+    #     # to a 2d array
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_mutationCountArrays(self,A,fitType):
+    #     # The method get_mutationCountArrays pads mutation count arrays and 
+    #     # increments the mutations counts for the new entries, depending on
+    #     # whether the array is for abs or rel fitness mutations.
+    
+    # #------------------------------------------------------------------------------
+
+    # def get_evoArraysCollapse(self):
+    #     # get_evoArraysCollapse() collapses evo arrays to remove any rows/cols
+    #     # with only zero entries
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_trimIndicesForZeroRows(self,A):
+    #     # generic function to trim rows with zeros from top and bottom of A
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_stochasticDynamicsCutoff(self):
+    #     # calc_StochThreshold determines abundance for establishment of class
+    #     # to be used in full and approximate simulation.
+    
+    # # --------------------------------------------------------------------------
+    # # Methods for Approximate Stochastic Model 
+    # # --------------------------------------------------------------------------
+
+    # def run_determinsticEvolution(self):
+    #     # main method to run the model
+    
+    # #------------------------------------------------------------------------------
+
+    # def run_poissonSamplingOfAbundances(self):
+    #     # we run a simple poisson sampling for abundances with mean equal to 
+    #     # values calculated from the density-dependent lottery model computations
+
+    # # --------------------------------------------------------------------------
+    # # General Methods
+    # #------------------------------------------------------------------------------
+    
+    # def get_bbar(self):
+    #     # get_bbar() returns population mean value of the b-terms
+    #     #    Note: requires implementation of bij
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_dbar(self):
+    #     # get_dbar() returns population mean value of the d-terms
+    #     #    Note: requires implementation of dij
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_cbar(self):
+    #     # get_cbar() returns population mean value of the c-terms
+    #     #    Note: requires implementation of cij
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_U(self):    
+    #     # get_U() returns the number of unoccupied territories
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_mij_noMutants(self):
+    #     # get_mij() returns the expected juveniles, but does not include mutations
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_lij(self):
+    #     # get_lij() returns the lottery model lij parameters for each genotype, 
+    #     # which are the poisson juvenile birth rates over unoccupied territories 
+    
+    # #------------------------------------------------------------------------------
+    
+    # def get_L(self):
+    #     # get_L() returns the lottery model L parameter (sum of the poisson birth 
+    #     # parameters over non-occupied territories)
+    
+    # #------------------------------------------------------------------------------
+    
+    # def clear_juvenilePop_mij(self):
+    #     # reset all juvenile counts to zero
+
+    # #------------------------------------------------------------------------------
+    
+    # def output_evoStats(self,ti):
+    #     # The method output_evoStats will collect all of the mean values of bij, cij,
+    #     # dij, and popsize
+    
+    # #------------------------------------------------------------------------------
+    
+    # def store_evoSnapshot(self):       
+    #     #  Capture the final snapshot, use the simInit class features and save
+    #     # a new simInit drived from the prior one
